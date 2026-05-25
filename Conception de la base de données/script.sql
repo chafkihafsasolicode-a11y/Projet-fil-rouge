b7788a12315897261e52f8099f9fc48005ed12ca
https://github.com/chafkihafsasolicode-a11y <chafkihafsa.solicode@gmail.com>
2026-05-25 09:20:44 +0100

    Conception de base

---
+++ Conception de la base de données/script.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Utilisateur (
+    id_utilisateur INT AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL PRIMARY KEY,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'client'
+) ;
+
+CREATE TABLE Voiture (
+    id_voiture INT AUTO_INCREMENT PRIMARY KEY,
+    marque VARCHAR(50) NOT NULL,
+    modele VARCHAR(50) NOT NULL,
+    type_carburant VARCHAR(30) NOT NULL,
+    boite_vitesse VARCHAR(30) NOT NULL,
+    tarif_journalier DECIMAL(10,2) NOT NULL,
+    disponibilite VARCHAR(20) NOT NULL DEFAULT 'disponible',
+    image VARCHAR(255),
+    annee INT NOT NULL
+) ;
+
+CREATE TABLE Reservation (
+    id_reservation INT AUTO_INCREMENT PRIMARY KEY,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+
+    statut_reservation VARCHAR(30) NOT NULL DEFAULT 'En attente',
+    id_utilisateur INT NOT NULL,
+    id_voiture INT NOT NULL,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur) ,
+    FOREIGN KEY (id_voiture) REFERENCES Voiture(id_voiture)
+);
